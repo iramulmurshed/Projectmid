46283779b61c12879f0b8f90dae21eecb30060aa
S.M.IRAMUL MURSHED <iramulmurshed@gmail.com>
2021-11-04 18:21:05 +0600

    final

--- seller/routes/web.php
+++ seller/routes/web.php
@@ -15,30 +15,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-    Route::get('/', [SellerController::class, 'viewSellerSignUpPage']) ->name('signup');
+Route::get('/', [SellerController::class, 'viewSellerSignUpPage']) ->name('signup');
 Route::post('/', [SellerController::class, 'verifySignup'])->name('signup');
 Route::get('/login', [SellerController::class, 'viewSellerLogInPage'])->name('login');
 Route::post('/login', [SellerController::class, 'verifyLogin'])->name('login');
 Route::get('/home', [SellerController::class, 'viewSellerHomePage'])
     ->name('home')->middleware('loginAuthentication');
+Route::get('/profile', [SellerController::class, 'profilePage'])
+    ->name('profile')->middleware('loginAuthentication');
+Route::post('/profileUpdate', [SellerController::class, 'profileUpdate'])
+    ->name('profileUpdate')->middleware('loginAuthentication');
+Route::get('/logout', [SellerController::class, 'sellerLogout'])->name('logout');
+
+
+
 Route::get('/add_product', [ProductController::class, 'addSellerProductPage'])
     ->name('add_product')->middleware('loginAuthentication');
 Route::post('/add_product', [ProductController::class, 'verifyProduct'])
     ->name('add_product')->middleware('loginAuthentication');
-
 Route::get('/show_product', [ProductController::class, 'viewProductPage'])
     ->name('show_product')->middleware('loginAuthentication');
-
 Route::get('/view_single_product/{p_id}', [ProductController::class, 'showSingleProduct'])
     ->name('show_single_product')->middleware('loginAuthentication');
-Route::get('/profile', [SellerController::class, 'profilePage'])
-    ->name('profile')->middleware('loginAuthentication');
 Route::get('/delete_single_product/{p_id}', [ProductController::class, 'deleteProduct'])
     ->name('delete_single_product')->middleware('loginAuthentication');
-Route::post('/profileUpdate', [SellerController::class, 'profileUpdate'])
-    ->name('profileUpdate')->middleware('loginAuthentication');
 Route::post('/updateProduct', [ProductController::class, 'updateProduct'])
     ->name('updateProduct')->middleware('loginAuthentication');
-Route::get('/logout', [SellerController::class, 'sellerLogout'])->name('logout');
+
 
 
